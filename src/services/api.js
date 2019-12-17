2531242c125b18e804d50700767538a131e2cbd9
Felipe de Oliveira Ribeiro <felipeoliveirasl@hotmail.com>
2019-12-17 16:35:42 -0300

    Add: api access

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const TOKEN = 'cc7e3a62b3b86860dc4f8d6e35edfb6a54f136ab';
-// const TOKEN = 'SEU_TOKEN';
+const TOKEN = 'SEU_TOKEN';
 
 const api = axios.create({
   baseURL: 'https://api.github.com/',
